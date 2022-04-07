d0fd136c0c1ad094da21cc2aef1cd4649565d46d
t_kobayashi <t_kobayashi@MacBook-Pro.local>
2022-04-07 16:58:53 +0900

    update

---
+++ src/pages/_document.tsx
@@ -0,0 +1,37 @@
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from "next/document";
+import React, { useState } from "react";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang="ja">
+        <Head>
+          <link rel="shortcut icon" type="image/x-icon" href="/favicon.png" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;700&family=Vujahday+Script&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body id="body">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
