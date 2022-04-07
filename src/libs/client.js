5df312b13dfa25a90104ef5b13d46b479ca8c300
t_kobayashi <t_kobayashi@MacBook-Pro.local>
2022-04-07 17:09:15 +0900

    update

---
+++ src/libs/client.js
@@ -0,0 +1,25 @@
+import { createClient } from "microcms-js-sdk";
+
+export const client = createClient({
+  serviceDomain: "sample-sample-sample",
+  apiKey: process.env.MICROCMS_API_KEY,
+});
+
+const getAllContents = async (offset = 0, limit = 10) => {
+  const data = await client.get({
+    endpoint: "diary",
+    queries: {
+      offset,
+      limit,
+    },
+  });
+
+  if (data.offset + data.limit <= data.totalCount) {
+    const contents = await getAllContents(data.offset + data.limit, data.limit);
+    return [...data.contents, ...contents];
+  }
+
+  return data.contents;
+};
+
+export { getAllContents };
